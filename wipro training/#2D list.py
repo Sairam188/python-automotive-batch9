08d22ffdf9f39a8d137ceb152700c253730bccf3
Sairam188 <sannapanenisairam@gmail.com>
2025-12-12 15:21:02 +0530

    firstcommit

---
+++ wipro training/#2D list.py
@@ -0,0 +1,11 @@
+#2D list
+
+ntr=["RRR","Devara","WAR2"]
+prabhas=["Baahubali","Baahubali2","Salaar"]
+heros=[ntr,prabhas]
+
+print(heros)
+print(heros[1])
+print(heros[0][1])
+
+
---
+++ wipro training/arithmetic.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter first Number"))
+num2=int(input("Enter Second Number"))
+
+#ADDITION
+add=num1+num2
+print("addition of given numbers is",add)
+
+#SUBTRACTION
+sub=num1-num2
+print("Subtraction of given numbers is",sub)
+
+#MULTIPLICATION
+mul=num1*num2
+print("Multiplication of given numbers is",mul) 
