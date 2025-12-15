d0cfb98d4b6f0c6e643762d089c4dc0063f6b337
Sairam188 <sannapanenisairam@gmail.com>
2025-12-15 16:41:09 +0530

    Create basicloops.py

---
+++ wipro automotive/basicloops.py
@@ -0,0 +1,21 @@
+while True:
+    name=input("Enter your name: ")
+    if name!="":
+        break
+print("hello",name)
+
+phone_number="123-456-7890"
+for i in phone_number:
+    if i=="-":
+        continue
+    print(i,end="")
+
+print("\n")
+
+for i in range(1,21):
+    if i==13:
+        pass
+    else:
+        print(i,end=",")
+
+
