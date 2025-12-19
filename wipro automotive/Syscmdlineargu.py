0e9cc7e7ebed25180cbfbb5652390b247c8a6b3c
Sairam188 <sannapanenisairam@gmail.com>
2025-12-19 09:14:33 +0530

    Update Syscmdlineargu.py

--- wipro automotive/Syscmdlineargu.py
+++ wipro automotive/Syscmdlineargu.py
@@ -2,11 +2,13 @@ import sys
 
 # Reading numbers from command-line arguments
 numbers = list(map(float, sys.argv[1:]))
+numbers = list(map(int, sys.argv[1:]))
 
 # Calculating sum and average
 total = sum(numbers)
 average = total / len(numbers)
 
 # Displaying results
+print("Numbers entered :", numbers)
 print("Sum:", total)
 print("Average:", average)
